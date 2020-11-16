1988d2993fdaa46dfb404e8f242aec0db2eb825b
Danteepl <danteepl@seznam.cz>
2020-11-16 22:38:26 +0100

    change1

--- src/main/java/olena/chorna/App.java
+++ src/main/java/olena/chorna/App.java
@@ -1,22 +1,4 @@
 package olena.chorna;
 
-public final class App {
-    public static void main(String[] args){
-        Zespolone a = new Zespolone(1,0);
-        Zespolone b = new Zespolone(0,1);
-        Zespolone c = a.dodaj(b);
-        System.out.println("re: " + c.re + ", im: " + c.im);
-    }
-}
-
-class Zespolone{
-    public int re, im;
-    public Zespolone(int re, int im) {
-        this.re = re;
-        this.im = im;
-    }
-    public Zespolone dodaj(Zespolone b) {
-        Zespolone c = new Zespolone(this.re + b.re, this.im + b.im );
-        return (c);
-    }
+/zmiana1
 }
